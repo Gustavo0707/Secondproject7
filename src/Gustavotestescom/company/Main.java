0168858189f9180e676b3da4a98ece725b39bb07
Gustavo0707 <gustavomelo0703@gmail.com>
2021-05-07 15:26:00 -0300

    .

--- src/Gustavotestescom/company/Main.java
+++ src/Gustavotestescom/company/Main.java
@@ -35,7 +35,7 @@ public class Main {
             System.out.println("the less number is " + number2);
         }
         else if (number3 < number1 && number3 < number2){
-            System.out.println("the lesse number is " + number3);
+            System.out.println("the less number is " + number3);
         }
 
     }
